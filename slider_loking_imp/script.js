63c6597eb0c19218e50736db1778f7f6b0f84600
Volodymyr Zhukivskyi <volodimirzukivskij@gmail.com>
2023-07-18 00:55:51 +0300

    change example, add setSlide btns

--- slider_loking_imp/script.js
+++ slider_loking_imp/script.js
@@ -1,17 +1,18 @@
 const buttonNext = document.querySelector(".next");
 const buttonPrev = document.querySelector(".prev");
 const slides = document.querySelectorAll("li");
-
+const btns = document.querySelector(".wrapper-btns").children
 function slider() {
   let count = 0;
+  showSlide(count)
   function showSlide(counter) {
     slides.forEach((el) => {
       const styleSlide = el.dataset.id == counter ? "block" : "none";
       el.style.display = styleSlide;
     });
   }
+
   function checkCounter() {
-    console.log(count);
     if (count > 3) {
       count = 0;
     } else if (count < 0) {
@@ -19,17 +20,21 @@ function slider() {
     }
     showSlide(count);
   }
-  return [
-    function nextSlide() {
-      count++;
+  return {
+    changeSlide: function (e) {
+      const nextBtnBool = e.target.id === "next";
+      nextBtnBool ? count++ : count--;
       checkCounter();
     },
-    function prevSlide() {
-      count--;
+    setCounter: function (value) {
+      count = value;
       checkCounter();
     },
-  ];
+  };
 }
-const [nextSlide, prevSlide] = slider();
-buttonNext.onclick = nextSlide;
-buttonPrev.onclick = prevSlide;
+const {changeSlide, setCounter} = slider();
+buttonNext.onclick = changeSlide;
+buttonPrev.onclick = changeSlide;
+for (const btn of btns) {
+btn.onclick=()=>setCounter(btn.dataset.id)
+}
