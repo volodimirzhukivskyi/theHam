65eb9db14ddf0d837186cda4fc577891310caf2b
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2024-01-02 21:02:42 +0200

    add adaptive

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
-const buttons = document.querySelectorAll(".tabs-title");
+const buttons = document.querySelectorAll('.tabs-title');
 buttons.forEach((btn) => {
-  btn.addEventListener("click", clickButton);
+  btn.addEventListener('click', clickButton);
 });
 
 function clickButton(event) {
-  let buttonAtr = this.getAttribute("data-id");
+  let buttonAtr = this.getAttribute('data-id');
   let text = document.querySelector(`.tabs-content [data-id = ${buttonAtr}]`);
-  document.querySelector(".show").classList.remove("show");
-  text.classList.add("show");
-  document.querySelector(".tabs-title.active").classList.remove("active");
-  this.classList.add("active");
+  document.querySelector('.show').classList.remove('show');
+  text.classList.add('show');
+  document.querySelector('.tabs-title.active').classList.remove('active');
+  this.classList.add('active');
 }
 
 ///////////////////////////////////////////////////////////////////////
-const buttonsImg = document.querySelectorAll(".tabs-title-img");
+const buttonsImg = document.querySelectorAll('.tabs-title-img');
 buttonsImg.forEach((btn) => {
-  btn.addEventListener("click", clickButtonImg);
+  btn.addEventListener('click', clickButtonImg);
 });
 
 function clickButtonImg(event) {
-  let buttonAtr = this.getAttribute("data-id");
-  document.querySelector(".tabs-title-img.active").classList.remove("active");
-  this.classList.add("active");
+  let buttonAtr = this.getAttribute('data-id');
+  document.querySelector('.tabs-title-img.active').classList.remove('active');
+  this.classList.add('active');
   printSearchElements(buttonAtr);
 }
 
 function creatArrsUrl(category, lengs) {
-  const changeStr = category.split("-").join(" ");
+  const changeStr = category.split('-').join(' ');
   let result = [];
   for (let i = 1; i <= lengs; i++) {
     result.push({
@@ -39,61 +39,58 @@ function creatArrsUrl(category, lengs) {
 
 const data = [
   {
-    dataId: "GraphicDesign",
-    arr: creatArrsUrl("graphic-design", 6),
+    dataId: 'GraphicDesign',
+    arr: creatArrsUrl('graphic-design', 6),
   },
   {
-    dataId: "All",
+    dataId: 'All',
+    arr: [...creatArrsUrl('graphic-design', 6), ...creatArrsUrl('landing-page', 6)],
+  },
+  {
+    dataId: 'LandingPages',
+    arr: creatArrsUrl('landing-page', 6),
+  },
+  {
+    dataId: 'AllPics',
     arr: [
-      ...creatArrsUrl("graphic-design", 6),
-      ...creatArrsUrl("landing-page", 6),
+      ...creatArrsUrl('graphic-design', 6),
+      ...creatArrsUrl('landing-page', 6),
+      ...creatArrsUrl('wordpress', 6),
+      ...creatArrsUrl('web-design', 6),
     ],
   },
   {
-    dataId: "LandingPages",
-    arr: creatArrsUrl("landing-page", 6),
+    dataId: 'Wordpress',
+    arr: creatArrsUrl('wordpress', 6),
   },
   {
-    dataId: "AllPics",
-    arr: [
-      ...creatArrsUrl("graphic-design", 6),
-      ...creatArrsUrl("landing-page", 6),
-      ...creatArrsUrl("wordpress", 6),
-      ...creatArrsUrl("web-design", 6),
-    ],
-  },
-  {
-    dataId: "Wordpress",
-    arr: creatArrsUrl("wordpress", 6),
-  },
-  {
-    dataId: "WebDesign",
-    arr: creatArrsUrl("web-design", 6),
+    dataId: 'WebDesign',
+    arr: creatArrsUrl('web-design', 6),
   },
 ];
 function loadMore(element) {
   console.log(this);
-  const loaderIcon = element.querySelector("I");
-  const img = element.querySelector("IMG");
-  loaderIcon.style.cssText="display:block;margin-right:5px";
-  img.style.display="none";
+  const loaderIcon = element.querySelector('I');
+  const img = element.querySelector('IMG');
+  loaderIcon.style.cssText = 'display:block;margin-right:5px';
+  img.style.display = 'none';
   setTimeout(() => {
-    loaderIcon.style.display="none";
-    const allButton = document.querySelector(".tabs-title-img.active");
-    allButton.dataset.id = "AllPics";
-    printSearchElements("AllPics");
+    loaderIcon.style.display = 'none';
+    const allButton = document.querySelector('.tabs-title-img.active');
+    allButton.dataset.id = 'AllPics';
+    printSearchElements('AllPics');
     element.remove();
   }, 3000);
 }
 
 function printSearchElements(categoryId) {
-  const imgWrapper = document.querySelector(".img-content");
-  const button = document.querySelector(".load-more");
-  imgWrapper.textContent = "";
+  const imgWrapper = document.querySelector('.img-content');
+  const button = document.querySelector('.load-more');
+  imgWrapper.textContent = '';
   const currentObj = data.find((obj) => obj.dataId === categoryId);
   currentObj.arr.forEach((obj) => {
     imgWrapper.insertAdjacentHTML(
-      "afterbegin",
+      'afterbegin',
       ` <figure class="product">
                 <img class="product-img" src= ${obj.url}>
                 <div class="hover-product">
@@ -112,76 +109,74 @@ function printSearchElements(categoryId) {
                     <p class="product-text">${obj.name}</p>
 
                 </div>
-            </figure`,
+            </figure`
     );
   });
-  if (categoryId === "All" && button === null) {
+  if (categoryId === 'All' && button === null) {
     imgWrapper.insertAdjacentHTML(
-      "afterend",
+      'afterend',
 
-      `<button class="button-add load-more" onclick="loadMore(this)"><i class="fa fa-refresh fa-spin"></i> <img src="./img/icons/forma1.png" alt=""/>Load More</button>`,
+      `<button class="button-add load-more" onclick="loadMore(this)"><i class="fa fa-refresh fa-spin"></i> <img src="./img/icons/forma1.png" alt=""/>Load More</button>`
     );
   } else {
     button?.remove();
   }
 }
 
-printSearchElements("All");
+printSearchElements('All');
 
 let bullet = [
-  "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxM[…]2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-  "https://i.pinimg.com/originals/f3/13/07/f313074df0a3210dc7ca4b7e97f7491c.jpg",
-  "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxM[…]HxzZWFyY2h8MTJ8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&w=1000&q=80",
+  'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxM[…]2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+  'https://i.pinimg.com/originals/f3/13/07/f313074df0a3210dc7ca4b7e97f7491c.jpg',
+  'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxM[…]HxzZWFyY2h8MTJ8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+  'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&w=1000&q=80',
 ];
-let swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
 
     renderBullet: function (index, className) {
-      return `<div class='${className + " custom-bullet"}'><img src=${
-        bullet[index]
-      }></div>`;
+      return `<div class='${className + ' custom-bullet'}'><img src=${bullet[index]}></div>`;
     },
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
 
 ///////////////////////////////////////////////////////////////
-var grid = document.querySelector(".grid");
+var grid = document.querySelector('.grid');
 var msnry = new Masonry(grid, {
-  itemSelector: ".grid-item",
-  columnWidth: ".grid-sizer",
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
   percentPosition: true,
   gutter: 20,
-  transitionDuration: "0.8s",
+  transitionDuration: '0.8s',
 });
-var subGrid = document.querySelector(".sub-grid");
+var subGrid = document.querySelector('.sub-grid');
 var msnrysub = new Masonry(subGrid, {
-  itemSelector: ".subgrid-item",
+  itemSelector: '.subgrid-item',
   gutter: 3,
-  columnWidth: ".subgrid-sizer",
-  transitionDuration: "0.2s",
+  columnWidth: '.subgrid-sizer',
+  transitionDuration: '0.2s',
 });
-var subGrid2 = document.querySelector(".sub-grid2");
+var subGrid2 = document.querySelector('.sub-grid2');
 
 var msnrysub2 = new Masonry(subGrid2, {
-  itemSelector: ".subgrid-item2",
+  itemSelector: '.subgrid-item2',
   gutter: 3,
-  columnWidth: ".subgrid-sizer2",
-  transitionDuration: "0.2s",
+  columnWidth: '.subgrid-sizer2',
+  transitionDuration: '0.2s',
 });
 ////////////////////////////////////////////////////////////////
-const btnMasonry = document.querySelector(".add-masonry-items");
-btnMasonry.addEventListener("click", function () {
+const btnMasonry = document.querySelector('.add-masonry-items');
+btnMasonry.addEventListener('click', function () {
   var elems = [];
   var fragment = document.createDocumentFragment();
   for (var i = 1; i < 5; i++) {
@@ -194,7 +189,7 @@ btnMasonry.addEventListener("click", function () {
   btnMasonry.remove();
 });
 function getItemElement(i) {
-  var elem = document.createElement("div");
+  var elem = document.createElement('div');
   (elem.innerHTML = `<div class="hide_block">
     <div class="hover_icon">
       <i class="fa fa-search fa-search-circle"></i>
@@ -204,19 +199,19 @@ function getItemElement(i) {
     </div>
   </div>
   <img src="./img/gallery/gallery${i}.jpg" alt="" />`),
-    (elem.className = "grid-item ");
+    (elem.className = 'grid-item ');
   return elem;
 }
 ////////////////////////////////////////////////////////////////////
-imagesLoaded(grid).on("progress", function () {
+imagesLoaded(grid).on('progress', function () {
   msnry.layout();
 });
 //////////////////////////////////////////////////////////////////
-grid.addEventListener("click", function (event) {
-  if (event.target.tagName !== "I") {
+grid.addEventListener('click', function (event) {
+  if (event.target.tagName !== 'I') {
     return;
   }
-  const selectors = ["subgrid-item2", "subgrid-item", "grid-item"];
+  const selectors = ['subgrid-item2', 'subgrid-item', 'grid-item'];
   for (const selector of selectors) {
     const parent = event.target.closest(`.${selector}`);
     if (parent) {
@@ -228,4 +223,15 @@ grid.addEventListener("click", function (event) {
   msnry.layout();
   msnrysub.layout();
   msnrysub2.layout();
+});
+//////////////////////////////////////Burger Menu //////////////////////
+
+const button = document.querySelector('.navigation__button');
+button.addEventListener('click', (e) => {
+const button = e.target.closest(".navigation__button")
+const menu = document.querySelector('.navigation');
+
+button.classList.toggle('active');
+const method =button.classList.contain("active")?"add":"remove"
+menu.classList[method]('active');
 });
